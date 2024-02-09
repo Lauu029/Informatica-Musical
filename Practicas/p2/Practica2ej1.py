b656cbe47a3904c39f4535b3f520b79d6822dc46
miriam-m-s <81752837+miriam-m-s@users.noreply.github.com>
2024-02-09 16:39:47 +0100

    aaaaaaaaaa

---
+++ Practicas/p2/Practica2ej1.py
@@ -0,0 +1,81 @@
+# %%
+## Laura Gomez Bodego
+## Miriam Martin Sanchez
+#%%
+import numpy as np
+import sounddevice as sd
+import kbhit
+#%%
+SRATE = 48000
+CHUNK = 1024
+
+# %%
+class Osc:
+    
+    def __init__(self, frequency, volume, phase=0):
+        self.frequency = frequency
+        self.volume = volume
+        self.phase = phase
+        self.current_frame = 0
+        
+    def set_frequency(self, frequency):
+        self.frequency = frequency
+
+    def get_frequency(self):
+        return self.frequency
+
+    def set_volume(self, volume):
+        self.volume = volume
+
+    def get_volume(self):
+        return self.volume
+
+    def next(self):
+        data = self.volume*np.sin(2*np.pi*(np.arange(self.current_frame,self.current_frame+CHUNK))*self.frequency/SRATE)
+        self.current_frame += CHUNK # actualizamos ultimo generado
+        return np.float32(data)
+    
+#%%
+
+def testOsc():
+    kb = kbhit.KBHit()
+
+    numBloque = 0 # contador de bloques/chunks
+    end = False # será true cuando el chunk esté incompleto o se pare la reproducción
+
+    initial_frequency = 440.0  # Hz
+    initial_volume = 1.0
+    osc = Osc(initial_frequency, initial_volume)
+    # stream de salida
+    stream = sd.OutputStream( # creamos stream
+        samplerate = SRATE, # frec de muestreo
+        blocksize = CHUNK, # tamaño del bloque
+        channels = 1) # num de canales
+    stream.start() # arrancamos stream
+    while not(end):
+        
+        bloque = osc.next()
+     
+        # Reproducir el chunk utilizando sounddevice
+        
+        if kb.kbhit():
+            c = kb.getch() # variacion de volumen/abortar
+            if (c=='v'): osc.set_volume(max(0,osc.get_volume()-0.05))
+            elif (c=='V'):  osc.set_volume(min(1,osc.get_volume()+0.05))
+            elif (c=='f') : osc.set_frequency(max(20,osc.get_frequency()-10))
+            elif (c=='F') : osc.set_frequency(min(20000,osc.get_frequency()+10))
+            elif c in ['q','escape']: end = True
+        stream.write(bloque)
+        print(f"\rVol: {osc.get_volume():.2f} bloque: {numBloque}",end='')
+        #print(f"\rFrec: {osc.get_frequency():.2f} bloque: {numBloque}",end='')
+        numBloque += 1
+    kb.set_normal_term()
+    stream.stop() # cerramos stream
+    stream.close()
+
+
+#%%
+if __name__ == "__main__":
+    testOsc()
+
+# %%
--- Practicas/p2/Practica2ej4.py
+++ Practicas/p2/Practica2ej4.py
@@ -35,48 +35,7 @@ class Osc:
         self.current_frame += CHUNK # actualizamos ultimo generado
         return np.float32(data)
     
-#%%
 
-def testOsc():
-    kb = kbhit.KBHit()
-
-    numBloque = 0 # contador de bloques/chunks
-    end = False # será true cuando el chunk esté incompleto o se pare la reproducción
-
-    initial_frequency = 440.0  # Hz
-    initial_volume = 1.0
-    osc = Osc(initial_frequency, initial_volume)
-    # stream de salida
-    stream = sd.OutputStream( # creamos stream
-        samplerate = SRATE, # frec de muestreo
-        blocksize = CHUNK, # tamaño del bloque
-        channels = 1) # num de canales
-    stream.start() # arrancamos stream
-    while not(end):
-        
-        bloque = osc.next()
-     
-        # Reproducir el chunk utilizando sounddevice
-        
-        if kb.kbhit():
-            c = kb.getch() # variacion de volumen/abortar
-            if (c=='v'): osc.set_volume(max(0,osc.get_volume()-0.05))
-            elif (c=='V'):  osc.set_volume(min(1,osc.get_volume()+0.05))
-            elif (c=='f') : osc.set_frequency(max(20,osc.get_frequency()-10))
-            elif (c=='F') : osc.set_frequency(min(20000,osc.get_frequency()+10))
-            elif c in ['q','escape']: end = True
-        stream.write(bloque)
-        print(f"\rVol: {osc.get_volume():.2f} bloque: {numBloque}",end='')
-        #print(f"\rFrec: {osc.get_frequency():.2f} bloque: {numBloque}",end='')
-        numBloque += 1
-    kb.set_normal_term()
-    stream.stop() # cerramos stream
-    stream.close()
-
-
-#%%
-##if __name__ == "__main__":
-    ##testOsc()
 # %%
 class Modulator(Osc):
     def __init__(self, frequency, volume, phase=0, min_value=-1, max_value=1, balance=0):
@@ -86,11 +45,15 @@ class Modulator(Osc):
         self.balance = balance
 
     def set_balance(self, balance):
+        ## la variable balance solo puede estar en el rango -1 u 1 
+        ## -1 canal izquierdo maximo
+        ##1 canal derecho maximo
         self.balance = np.clip(balance, -1, 1)
 
     def next(self):
         data = super().next()
         modulated_data = self.min_value + (self.max_value - self.min_value) * (1 + self.balance) * data / 2
+        ## aumentamos el sonido del canal izquierdo 
         left_channel = modulated_data * (1 - np.abs(self.balance))
         right_channel = modulated_data * (1 + np.abs(self.balance))
         return np.vstack([left_channel, right_channel]).T.astype(np.float32)
@@ -132,7 +95,7 @@ def testModulator():
             elif c in ['q', 'escape']:
                 end = True
 
-        stream.write(bloque)
+        stream.write(np.ascontiguousarray(bloque))
         print(f"\rVol: {modulator.get_volume():.2f} Frec: {modulator.get_frequency():.2f} Balance: {modulator.balance:.2f} Bloque: {numBloque}", end='')
         numBloque += 1
 
