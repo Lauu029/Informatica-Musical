e480e772da57e9ef92f029fcd52ba06907cbc6d4
Lauu029 <lgomez25@ucm.es>
2024-02-29 15:07:59 +0100

    y nos la pela

---
+++ Practicas/p5/osc.py
@@ -0,0 +1,18 @@
+
+from consts import *
+import numpy as np         
+import sounddevice as sd       
+import matplotlib.pyplot as plt
+
+class Osc:
+    def __init__(self,moduleOnda='sin',freq=440.0,amp=1.0,phase=0.0):
+        self.freq = freq
+        self.amp = amp
+        self.phase = phase
+        self.frame = 0
+        self.moduleOnda=moduleOnda
+
+    def next(self):    
+        out = self.amp*np.sin(2*np.pi*(np.arange(self.frame,self.frame+CHUNK))*self.freq/SRATE)
+        self.frame += CHUNK
+        return out
